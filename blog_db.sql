8ffca493daad40a0a2d43316e872da63c6c62f56
WhiteFoOx <pavelgordeev96@gmail.com>
2018-07-02 17:51:46 +0700

    add_db

--- blog_db.sql
+++ blog_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Июл 01 2018 г., 13:37
--- Версия сервера: 10.1.24-MariaDB
--- Версия PHP: 7.1.6
+-- Хост: localhost
+-- Время создания: Июл 02 2018 г., 12:51
+-- Версия сервера: 10.1.33-MariaDB
+-- Версия PHP: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,16 +32,10 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `text` text NOT NULL
+  `text` text NOT NULL,
+  `date` date NOT NULL,
+  `views` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `posts`
---
-
-INSERT INTO `posts` (`id`, `name`, `description`, `text`) VALUES
-(1, 'Ð¡Ð°Ð½Ñ', 'Ð¥Ð²Ð¾Ñ€Ð¾ÑÑ‚Ð¸Ð½Ð¸Ð½', 'ÐÐ»ÐµÐºÑÐ°Ð½Ð´Ñ€ Ð¥Ð²Ð¾Ñ€Ð¾ÑÑ‚Ð¸Ð½Ð¸Ð½, Ð¿Ð°Ñ€ÐµÐ½ÑŒ Ð½Ðµ Ð¿Ð»Ð¾Ñ…Ð¾Ð¹, Ð½Ð¾ Ð¾Ð½ Ð½Ðµ Ð·Ð°Ñ…Ð¾Ñ‚ÐµÐ» Ð¿Ð¸ÑÐ°Ñ‚ÑŒ Ñ‚ÐµÐºÑÑ‚ Ð¼Ð½Ðµ Ð´Ð»Ñ Ð±Ð»Ð¾Ð³Ð° Ð¿Ñ€Ð¾ ÑÐµÐ±Ñ. Ðš Ñ‚Ð¾Ð¼Ñƒ Ð¶Ðµ Ð¾Ð½ Ð¸Ð½Ð¾Ð³Ð´Ð° ÑÐ»Ð¸ÑˆÐºÐ¾Ð¼ ÑÐµÑ€ÑŒÐµÐ·Ð½Ð¾ ÑˆÑŒÐµÑ‚ Ð² Ð³Ð¾Ð»Ð¾Ð²Ñƒ Ñ€ÐµÐ±ÑÑ‚Ð°Ð¼ Ð¸Ð· Ð¸Ð³Ñ€Ñ‹ ÐºÐ¾Ð½Ñ‚ÐµÑ€ ÑÑ‚Ñ€Ð°Ð¹Ðº Ð³Ð»Ð¾Ð±ÐµÐ» Ð¾Ñ„ÐµÐ½ÑÐ¸Ð²'),
-(2, 'ÐÐ½Ñ‚Ð¾Ð½', 'Ð§ÐµÑ€ÐµÐ¿Ð°Ð½Ð¾Ð²', 'Ð§ÐµÑ€ÐµÐ¿Ð°Ð½Ð¾Ð² ÐÐ½Ñ‚Ð¾Ð½ - ÑÑ‚Ð¾ Ñ‚Ð°ÐºÐ¾Ð¹ Ð¿Ð°Ñ€Ñ, ÐºÐ¾Ñ‚Ð¾Ñ€Ñ‹Ð¹ Ð½Ð° Ð²Ñ‹Ñ…Ð¾Ð´Ð½Ñ‹Ñ… Ð·Ð²Ð°Ð» Ð¼ÐµÐ½Ñ ÐºÐ°Ñ‚Ð°Ñ‚ÑŒ Ð¸ Ñ„Ð¾Ñ€Ñ‚Ð½Ð°ÐµÑ‚ Ð¸ Ð»Ð¸Ð³Ñƒ Ð»ÐµÐ³ÐµÐ½Ð´, Ñ‚ÐµÐ¼ ÑÐ°Ð¼Ñ‹Ð¼ Ð¼ÐµÑˆÐ°Ñ Ð¼Ð½Ðµ Ð´ÐµÐ»Ð°Ñ‚ÑŒ ÑÐ²Ð¾ÑŽ Ñ€Ð°Ð±Ð¾Ñ‚Ñƒ.');
 
 --
 -- Индексы сохранённых таблиц
@@ -61,7 +55,8 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
